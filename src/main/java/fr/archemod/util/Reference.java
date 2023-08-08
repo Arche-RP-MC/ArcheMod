8f4c249bf3c52b3d3201d16b3aa8b5fa46941688
martini29u <florent.martini2@etu.univ-lorraine.fr>
2023-08-08 14:41:53 +0200

    Version 3.18

--- src/main/java/fr/archemod/util/Reference.java
+++ src/main/java/fr/archemod/util/Reference.java
@@ -3,7 +3,7 @@ package fr.archemod.util;
 public class Reference {
     public static final String MOD_ID = "am";
     public static final String MOD_NAME = "ArcheMod";
-    public static final String VERSION = "3.12";
+    public static final String VERSION = "3.18";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
     public static final String CLIENT_PROXY_CLASS = "fr.archemod.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "fr.archemod.proxy.CommonProxy";
