3598b961c889eacc7a672685d013d1fc37ac565d
Saresttoi <80989075+Saresttoi@users.noreply.github.com>
2023-10-26 23:17:11 +0200

    Grosse maj réorganisation de la classe plante en BlocCulture

--- src/main/java/fr/archemod/blocks/BlocCulture.java
+++ src/main/java/fr/archemod/blocks/BlocCulture.java
@@ -1,4 +1,4 @@
-package fr.archemod.blocks.cultures;
+package fr.archemod.blocks;
 
 import fr.archemod.ArcheMod;
 import fr.archemod.init.ModBlocks;
@@ -16,23 +16,30 @@ import net.minecraft.world.World;
 
 import java.util.Random;
 
-//Pour plantes racines récoltées à la fourche ou à la bêche
-public class BlocPlantesRacines extends BlockCrops {
+
+public class BlocCulture extends BlockCrops {
+
+    public static Item[] serpes = new Item[]{ModItems.SERPE_EN_FER, ModItems.SERPE_EN_ACIER};
+    public static Item[] faux = new Item[]{ModItems.FAUX};
+    public static Item[] cisailles = new Item[]{ModItems.PETITE_CISAILLE, Items.SHEARS};
+    public static Item[] fourche = new Item[]{ModItems.FOURCHE_EN_FER, Items.IRON_SHOVEL, Items.IRON_HOE};
+    public static Item[] main = new Item[]{ModItems.COUTEAU_EN_FER, ModItems.COUTEAU_ACIER};//TODO : faire fonctionner pour la récolte sans outil, pas encore trouvé
 
     private final Item seed;
     private final Item crop;
+    private final Item[] tools;
     private final int dropsWhenMature;
     private final int dropsWhenRightClicked;
     private final int dropsWhenBroken;
 
-
-    public BlocPlantesRacines(String name, Item seed, Item crop, int dropsWhenMature, int dropsWhenRightClicked, int dropsWhenBroken) {
+    public BlocCulture(String name, Item seed, Item crop, Item[] tools, int dropsWhenMature, int dropsWhenRightClicked, int dropsWhenBroken) {
         setTranslationKey(name);
         setRegistryName(name);
         setCreativeTab(ArcheMod.archeCreativeTabs);
 
         this.seed = seed;
         this.crop = crop;
+        this.tools = tools; //Outils de récolte : faux, serpe, petite cisaille, pelle, bêche etc...
         this.dropsWhenMature = dropsWhenMature;
         this.dropsWhenRightClicked = dropsWhenRightClicked;
         this.dropsWhenBroken = dropsWhenBroken;
@@ -57,27 +64,30 @@ public class BlocPlantesRacines extends BlockCrops {
         Random rand = world instanceof World ? ((World) world).rand : new Random();
 
         if (age >= getMaxAge()) {
-            drops.add(new ItemStack(seed, dropsWhenMature/*+ rand.nextInt(dropsWhenRightClicked + 1)*/, 0));
-            drops.add(new ItemStack(crop, dropsWhenMature/*+ rand.nextInt(dropsWhenRightClicked + 1)*/, 0));
+            drops.add(new ItemStack(seed, dropsWhenMature /*+ rand.nextInt(dropsWhenRightClicked + 1)*/, 0));
+            drops.add(new ItemStack(crop, dropsWhenMature /*+ rand.nextInt(dropsWhenRightClicked + 1)*/, 0));
         }
     }
 
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
         ItemStack heldItem = player.getHeldItem(hand);
 
-        if (heldItem.getItem() == ModItems.FOURCHE_EN_FER || heldItem.getItem() == Items.IRON_HOE || heldItem.getItem() == Items.IRON_SHOVEL) {
-            if (!worldIn.isRemote) {
-                int age = getAge(state);
-                if (age >= getMaxAge()) {
-                    worldIn.setBlockToAir(pos);
-                    spawnAsEntity(worldIn, pos, new ItemStack(crop, dropsWhenRightClicked));
-                    heldItem.damageItem(1, player);
+        for (Item tool : tools) {
+            if (heldItem.getItem() == tool) {
+                if (!worldIn.isRemote) {
+                    int age = getAge(state);
+                    if (age >= getMaxAge()) {
+                        worldIn.setBlockState(pos, withAge(0));
+                        spawnAsEntity(worldIn, pos, new ItemStack(crop, dropsWhenRightClicked));
+                        heldItem.damageItem(1, player);
+                    }
                 }
+                return true;
             }
-            return true;
         }
 
         return super.onBlockActivated(worldIn, pos, state, player, hand, facing, hitX, hitY, hitZ);
-
     }
+
+
 }
